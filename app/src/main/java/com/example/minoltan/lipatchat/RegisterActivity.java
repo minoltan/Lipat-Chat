54b6cc6977a524d4cc868b48ae9db10e405e1d2e
minoltan <minoltan_se15021@kln.ac.lk>
2018-11-28 22:58:12 +0530

    app started with authentication

---
+++ app/src/main/java/com/example/minoltan/lipatchat/RegisterActivity.java
@@ -0,0 +1,117 @@
+package com.example.minoltan.lipatchat;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+
+private TextInputLayout mName;
+private TextInputLayout mEmail;
+private TextInputLayout mPassword;
+private Button mCreateBtn;
+
+    private DatabaseReference mDatabase;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        mName = (TextInputLayout) findViewById(R.id.reg_display_name);
+        mEmail = (TextInputLayout) findViewById(R.id.reg_email);
+        mPassword =(TextInputLayout) findViewById(R.id.reg_password);
+
+
+
+        mCreateBtn = (Button) findViewById(R.id.reg_create_btn);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("User_01");
+
+
+       /* mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mName.getEditText().getText().toString().trim();
+                String email = mEmail.getEditText().getText().toString().trim();
+
+                HashMap<String,String> datamap = new HashMap<String, String>();
+                datamap.put("Name", name);
+                datamap.put("Email", email);
+
+
+                mDatabase.push().setValue(datamap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "Registered Succesfully", Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(RegisterActivity.this, "Registere Error", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+                Intent intent = new Intent(RegisterActivity.this,StartActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });*/
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String display_name = mName.getEditText().getText().toString();
+                String email = mEmail.getEditText().getText().toString();
+                String password = mPassword.getEditText().getText().toString();
+
+                register_user(display_name, email, password);
+
+            }
+        });
+
+    }
+
+    private void register_user(String display_name, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                            startActivity(mainIntent);
+                            finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+
+                });
+
+    }
+}
